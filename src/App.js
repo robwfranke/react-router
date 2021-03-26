45f291ce1074b6c54ad2363b33c9b8432be5775f
=Rob Franke <robwfranke@gmail.com>
2021-03-26 13:46:58 +0100

    via butto op details page terug naar homepage, useHistory

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import {
     Switch,
     Route,
     NavLink,
-    Link
+    Link,
+    useHistory
 } from 'react-router-dom';
 
 import './App.css';
@@ -15,6 +16,19 @@ import Details from "./pages/Details";
 function App() {
     // zet router element in index.js
 
+    const history = useHistory();
+
+
+    function handleClick(){
+        console.log("Je hebt geklikt");
+//     doorlinken naar de home pagina
+        history.push("/")
+
+
+    }
+
+
+
     return (
         <>
             <nav>
@@ -24,11 +38,15 @@ function App() {
                     </li>
 
                     <li>
-                        <NavLink to="/details-cursus" activeClassName="active-link">Details-Cursus Page</NavLink>
+                        <NavLink to="/details-cursus" activeClassName="active-link">
+                            Details-Cursus Page (geef idCursus mee) detais-cursus/idCursus
+
+                        </NavLink>
                     </li>
                 </ul>
-
             </nav>
+
+
 
             <Switch>
                 {/* eslint-disable-next-line react/jsx-no-undef */}
@@ -43,9 +61,25 @@ function App() {
                 </Route>
 
 
+
+
                 <Route exact path="/details">
-                    <Details/>
+                    <section>
+                        <h1> details page</h1>
+                        <button onClick={handleClick} type = "button">Klik hier!</button>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aliquam animi at commodi deleniti dolor doloremque dolores eaque eius et eum excepturi expedita fugit, laboriosam laborum, magnam maxime natus necessitatibus numquam officiis perspiciatis placeat quae quaerat quia quisquam quod rem rerum sapiente temporibus tenetur totam ut velit veniam vero vitae voluptas voluptate. Accusamus architecto asperiores dicta dolore ducimus illo illum, iusto minima, molestias mollitia obcaecati, similique vel voluptatum. Adipisci at atque debitis deserunt illum laboriosam minima molestias nobis officiis omnis possimus quae, quidem repellendus saepe sequi sint ut vel vero? Aspernatur beatae consequuntur corporis dicta ea enim eos esse, est et eum laudantium minus natus nisi non numquam quod, recusandae repellendus sit, unde voluptatem! A ab accusamus aliquid consequatur distinctio, doloribus eius fugiat harum illo, illum laboriosam maiores minima molestias neque nisi nulla numquam officia omnis porro quam repudiandae saepe sed ullam voluptatem voluptatum. Blanditiis cupiditate dolores ea eaque eum fugit nihil officiis porro ratione tenetur. Aut enim maiores perferendis quo repellendus? Accusantium adipisci at atque deleniti dolores, expedita in ipsam nemo, numquam, quasi ratione reiciendis rem repellendus temporibus tenetur voluptate voluptates. Asperiores aspernatur cum, delectus non numquam quo repellat. Culpa doloremque ex facilis neque nostrum odio perspiciatis quae quisquam!
+                        </p>
+                        <Link to="/">Terug naar HomePage</Link>
+                    </section>
+
+
+
                 </Route>
+
+
+
+
 
                 <Route  path="/details-cursus/:idCursus">
 
@@ -55,12 +89,12 @@ function App() {
                     */}
 
                     <DetailsCursus/>
+
+
                 </Route>
 
 
             </Switch>
-
-
         </>
 
 
